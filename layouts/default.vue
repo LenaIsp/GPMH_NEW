55aa52cad27d90d4ef4e78bc8d865ce90e65ef7f
Elena Ispravnikova <elena-isp@192.168.0.84>
2023-09-23 16:53:42 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/business">Бизнес</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/media">Медиацентр</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">О нас</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/contacts">Контакты</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Eng</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <slot></slot>
+  </div>
+</template>
---
+++ pages/about/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Газпром-медиа - О нас</h2>
+  </div>
+</template>
